5542ac8d6f608c28bc518aeb05ec73cdd7e8c170
Sergii Siryi <sergii.siryi97@gmail.com>
2023-09-04 14:03:51 +0200

    fix migration sqls

--- src/Infrastructure/Persistence/Migrations/Version20230204211526.php
+++ src/Infrastructure/Persistence/Migrations/Version20230204211526.php
@@ -22,13 +22,13 @@ final class Version20230204211526 extends AbstractMigration
         $this->addSql("
             CREATE TABLE `categories`
             (
-                `id`            varchar(36)               not null,
-                `user_id`       varchar(36)               not null,
-                `type`          enum('expense', 'income') not null,
-                `name`          varchar(255)              not null,
-                `icon`          varchar(255)              not null,
-                `created_at`    datetime default NOW()    not null,
-                `updated_at`    datetime                  null,
+                `id` varchar(36) COLLATE utf8mb4_unicode_ci not null,
+                `user_id` varchar(36) not null,
+                `type` enum('expense', 'income') not null,
+                `name` varchar(255) not null,
+                `icon` varchar(255) not null,
+                `created_at` datetime default NOW() not null,
+                `updated_at` datetime null,
                 constraint categories_pk primary key (`id`),
                 constraint categories_user_id_name_type_unique unique (`user_id`, `name`, `type`)
             );
