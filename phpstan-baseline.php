d3b51c7bceb74cdff2e8ea4402ebb5bd0088faa8
Sergii Siryi <sergii.siryi97@gmail.com>
2023-09-21 20:47:33 +0200

    feat(Category|Wallet|Transaction)[Infrastructure] - update phpstan to level 9 and generate baseline

--- phpstan-baseline.php
+++ phpstan-baseline.php
@@ -2,9 +2,239 @@
 
 $ignoreErrors = [];
 $ignoreErrors[] = [
+	'message' => '#^Parameter \\#1 \\$id of class App\\\\Application\\\\Category\\\\GetAll\\\\CategoryDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Category/GetAll/GetAllCategoriesHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#2 \\$type of class App\\\\Application\\\\Category\\\\GetAll\\\\CategoryDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Category/GetAll/GetAllCategoriesHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#3 \\$name of class App\\\\Application\\\\Category\\\\GetAll\\\\CategoryDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Category/GetAll/GetAllCategoriesHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#4 \\$icon of class App\\\\Application\\\\Category\\\\GetAll\\\\CategoryDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Category/GetAll/GetAllCategoriesHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#1 \\$id of class App\\\\Application\\\\Category\\\\GetOneById\\\\CategoryDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Category/GetOneById/GetOneCategoryByIdHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#2 \\$type of class App\\\\Application\\\\Category\\\\GetOneById\\\\CategoryDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Category/GetOneById/GetOneCategoryByIdHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#3 \\$name of class App\\\\Application\\\\Category\\\\GetOneById\\\\CategoryDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Category/GetOneById/GetOneCategoryByIdHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#4 \\$icon of class App\\\\Application\\\\Category\\\\GetOneById\\\\CategoryDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Category/GetOneById/GetOneCategoryByIdHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#5 \\$date of static method App\\\\Domain\\\\Transaction\\\\Transaction\\:\\:create\\(\\) expects DateTimeImmutable, DateTimeImmutable\\|false given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Transaction/Create/CreateTransactionHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Cannot call method format\\(\\) on DateTimeImmutable\\|false\\.$#',
+	'count' => 2,
+	'path' => __DIR__ . '/src/Application/Transaction/GetAll/GetAllTransactionsHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Cannot cast mixed to int\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Transaction/GetAll/GetAllTransactionsHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#1 \\$id of class App\\\\Application\\\\Transaction\\\\GetAll\\\\TransactionDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Transaction/GetAll/GetAllTransactionsHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#2 \\$type of class App\\\\Application\\\\Transaction\\\\GetAll\\\\TransactionDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Transaction/GetAll/GetAllTransactionsHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#3 \\$sourceWalletId of class App\\\\Application\\\\Transaction\\\\GetAll\\\\TransactionDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Transaction/GetAll/GetAllTransactionsHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#4 \\$targetWalletId of class App\\\\Application\\\\Transaction\\\\GetAll\\\\TransactionDTO constructor expects string\\|null, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Transaction/GetAll/GetAllTransactionsHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#5 \\$categoryId of class App\\\\Application\\\\Transaction\\\\GetAll\\\\TransactionDTO constructor expects string\\|null, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Transaction/GetAll/GetAllTransactionsHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#6 \\$date of class App\\\\Application\\\\Transaction\\\\GetAll\\\\TransactionDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Transaction/GetAll/GetAllTransactionsHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#7 \\$description of class App\\\\Application\\\\Transaction\\\\GetAll\\\\TransactionDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Transaction/GetAll/GetAllTransactionsHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Cannot cast mixed to int\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Transaction/GetOneById/GetOneTransactionByIdHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#1 \\$id of class App\\\\Application\\\\Transaction\\\\GetOneById\\\\TransactionDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Transaction/GetOneById/GetOneTransactionByIdHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#2 \\$type of class App\\\\Application\\\\Transaction\\\\GetOneById\\\\TransactionDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Transaction/GetOneById/GetOneTransactionByIdHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#3 \\$sourceWalletId of class App\\\\Application\\\\Transaction\\\\GetOneById\\\\TransactionDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Transaction/GetOneById/GetOneTransactionByIdHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#4 \\$targetWalletId of class App\\\\Application\\\\Transaction\\\\GetOneById\\\\TransactionDTO constructor expects string\\|null, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Transaction/GetOneById/GetOneTransactionByIdHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#5 \\$categoryId of class App\\\\Application\\\\Transaction\\\\GetOneById\\\\TransactionDTO constructor expects string\\|null, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Transaction/GetOneById/GetOneTransactionByIdHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#6 \\$date of class App\\\\Application\\\\Transaction\\\\GetOneById\\\\TransactionDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Transaction/GetOneById/GetOneTransactionByIdHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#7 \\$description of class App\\\\Application\\\\Transaction\\\\GetOneById\\\\TransactionDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Transaction/GetOneById/GetOneTransactionByIdHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#5 \\$date of method App\\\\Domain\\\\Transaction\\\\Transaction\\:\\:update\\(\\) expects DateTimeImmutable, DateTimeImmutable\\|false given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Transaction/Update/UpdateTransactionHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Cannot cast mixed to int\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Wallet/GetAll/GetAllWalletsHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#1 \\$id of class App\\\\Application\\\\Wallet\\\\GetAll\\\\WalletDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Wallet/GetAll/GetAllWalletsHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#2 \\$name of class App\\\\Application\\\\Wallet\\\\GetAll\\\\WalletDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Wallet/GetAll/GetAllWalletsHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#4 \\$currency of class App\\\\Application\\\\Wallet\\\\GetAll\\\\WalletDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Wallet/GetAll/GetAllWalletsHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Cannot cast mixed to int\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Wallet/GetOneById/GetOneWalletByIdHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#1 \\$id of class App\\\\Application\\\\Wallet\\\\GetOneById\\\\WalletDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Wallet/GetOneById/GetOneWalletByIdHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#2 \\$name of class App\\\\Application\\\\Wallet\\\\GetOneById\\\\WalletDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Wallet/GetOneById/GetOneWalletByIdHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#4 \\$currency of class App\\\\Application\\\\Wallet\\\\GetOneById\\\\WalletDTO constructor expects string, mixed given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Application/Wallet/GetOneById/GetOneWalletByIdHandler.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\$repositoryClass of attribute class Doctrine\\\\ORM\\\\Mapping\\\\Entity constructor expects class\\-string\\<Doctrine\\\\ORM\\\\EntityRepository\\<T of object\\>\\>\\|null, \'App\\\\\\\\Domain\\\\\\\\Category\\\\\\\\CategoryRepository\' given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Domain/Category/Category.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\$repositoryClass of attribute class Doctrine\\\\ORM\\\\Mapping\\\\Entity constructor expects class\\-string\\<Doctrine\\\\ORM\\\\EntityRepository\\<T of object\\>\\>\\|null, \'App\\\\\\\\Domain\\\\\\\\Transaction\\\\\\\\TransactionRepository\' given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Domain/Transaction/Transaction.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\$repositoryClass of attribute class Doctrine\\\\ORM\\\\Mapping\\\\Entity constructor expects class\\-string\\<Doctrine\\\\ORM\\\\EntityRepository\\<T of object\\>\\>\\|null, \'App\\\\\\\\Domain\\\\\\\\Wallet\\\\\\\\WalletRepository\' given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Domain/Wallet/Wallet.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method App\\\\Infrastructure\\\\Persistence\\\\Doctrine\\\\DoctrineCategoryRepository\\:\\:getById\\(\\) should return App\\\\Domain\\\\Category\\\\Category but returns object\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Infrastructure/Persistence/Doctrine/DoctrineCategoryRepository.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method App\\\\Infrastructure\\\\Persistence\\\\Doctrine\\\\DoctrineIdType\\:\\:convertToDatabaseValue\\(\\) should return string\\|null but returns mixed\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Infrastructure/Persistence/Doctrine/DoctrineIdType.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method App\\\\Infrastructure\\\\Persistence\\\\Doctrine\\\\DoctrineTransactionRepository\\:\\:getById\\(\\) should return App\\\\Domain\\\\Transaction\\\\Transaction but returns object\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Infrastructure/Persistence/Doctrine/DoctrineTransactionRepository.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method App\\\\Infrastructure\\\\Persistence\\\\Doctrine\\\\DoctrineUserIdType\\:\\:convertToDatabaseValue\\(\\) should return string\\|null but returns mixed\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Infrastructure/Persistence/Doctrine/DoctrineUserIdType.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method App\\\\Infrastructure\\\\Persistence\\\\Doctrine\\\\DoctrineWalletRepository\\:\\:getById\\(\\) should return App\\\\Domain\\\\Wallet\\\\Wallet but returns object\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Infrastructure/Persistence/Doctrine/DoctrineWalletRepository.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Cannot access property \\$headers on Symfony\\\\Component\\\\HttpFoundation\\\\Request\\|null\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Infrastructure/User/JWTTokenUserContext.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Parameter \\#3 \\$subject of function str_replace expects array\\|string, string\\|null given\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Infrastructure/User/JWTTokenUserContext.php',
+];
+$ignoreErrors[] = [
 	'message' => '#^Unsafe usage of new static\\(\\)\\.$#',
 	'count' => 2,
 	'path' => __DIR__ . '/src/SharedKernel/Id.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Cannot call method getResult\\(\\) on Symfony\\\\Component\\\\Messenger\\\\Stamp\\\\HandledStamp\\|null\\.$#',
+	'count' => 2,
+	'path' => __DIR__ . '/src/UI/API/Controller/WalletController.php',
 ];
 $ignoreErrors[] = [
 	'message' => '#^Call to an undefined method Symfony\\\\Component\\\\Validator\\\\ConstraintViolationListInterface\\:\\:getIterator\\(\\)\\.$#',
